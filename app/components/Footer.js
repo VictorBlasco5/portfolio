4f0a419c69bba5a05009eab2210f7ee5322d5fc4
Víctor Blasco <victor.blasco.17@gmail.com>
2025-09-11 18:38:09 +0200

    design footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,7 +1,23 @@
+import styles from '../../styles/footer.module.css';
+import { SiGithub, SiLinkedin } from 'react-icons/si';
+
 export default function Footer() {
   return (
-    <footer style={{ background: '#eee', textAlign: 'center', padding: '1rem', marginTop: '2rem' }}>
-      © {new Date().getFullYear()} Víctor Blasco - Todos los derechos reservados.
+    <footer className={styles.footer}>
+      <div className={styles.marginLeft}></div>
+
+      <p>© 2025 Víctor Blasco - Todos los derechos reservados.</p>
+
+      <div className={styles.social}>
+        <div className={styles.media}>
+          <a href="https://github.com/VictorBlasco5" target="_blank" rel="noopener noreferrer">
+            <SiGithub size={20} color="#414141ff" /> GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/victor-blasco-4b7588304/" target="_blank" rel="noopener noreferrer">
+            <SiLinkedin size={20} color="#414141ff" /> LinkedIn
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
